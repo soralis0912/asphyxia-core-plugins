e6c7435fd449e363ef8c28abd4484b118b967262
soralis0912 <soralis.nem@gmail.com>
2023-12-15 18:08:05 +0900

    add Qubell Extend

--- jubeat@asphyxia/utility/utils.ts
+++ jubeat@asphyxia/utility/utils.ts
@@ -4,6 +4,8 @@ export const getGameVersion = (info: EamuseInfo) =>
     const gameRevision: number = parseInt(modelInfo[4]);
     if (gameRevision >=2016033001 && gameRevision <= 2017060600){
         return jubeatVersions.QUBELL;
+    }else if(gameRevision == 2017061500){
+        return jubeatVersions.QUBELL; // Qubell Extend
     }else if(gameRevision >= 2017072604 && gameRevision <= 2018070901){
         return jubeatVersions.CLAN;
     }else if(gameRevision >= 2018090501 && gameRevision <= 2022052400){
